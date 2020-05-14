da22e1c50aee49c65ab1aeca1bae2a1ddd5e9521
Daniel <danielgsoftware@gmail.com>
2020-05-14 14:18:57 +0200

    :ok_hand: fixing cs fixer

---
+++ src/RunCsFixerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace LenderSpender\SubstituteClassBinding;
+
+
+class RunCsFixerTest {
+
+    // This class is added to test if PHP cs fixer runs correctly
+
+}
