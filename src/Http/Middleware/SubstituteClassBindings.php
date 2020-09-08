a8315bdf24fc966367d91cfa3746a6eb2077e762
BertvanHoekelen <BertvanHoekelen@users.noreply.github.com>
2020-09-08 09:55:55 +0000

    Apply php-cs-fixer changes (skipci)

--- src/Http/Middleware/SubstituteClassBindings.php
+++ src/Http/Middleware/SubstituteClassBindings.php
@@ -24,8 +24,8 @@ class SubstituteClassBindings
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
